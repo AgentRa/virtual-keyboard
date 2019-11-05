ac25d28a22eaa2ebbf221e2f0952140a32413aae
Aliaksandr Razhachkin <bubonik.san1993@gmail.com>
2019-11-06 01:46:04 +0300

    Feat: add ctrl+alt language change, correct shift letterCase, enter button method, backspace method

--- src/index.js
+++ src/index.js
@@ -3,21 +3,20 @@ import keys from './keys';
 
 
 const appState = {
-  lang: 'en',
+  lang: 'ru',
   shiftFlag: 'off',
 };
-
+const div = document.createElement('div');
+div.className = 'wrapper';
+document.body.append(div);
+const text = document.createElement('textarea');
+text.className = 'textarea';
+text.setAttribute('disabled', 'disabled');
+document.body.querySelector('div').append(text);
+const keyboard = document.createElement('div');
+keyboard.className = 'keyboard';
+document.body.querySelector('div').append(keyboard);
 function drawBoard() {
-  const div = document.createElement('div');
-  div.className = 'wrapper';
-  document.body.append(div);
-  const text = document.createElement('textarea');
-  text.className = 'textarea';
-  text.setAttribute('disabled', 'disabled');
-  document.body.querySelector('div').append(text);
-  const keyboard = document.createElement('div');
-  keyboard.className = 'keyboard';
-  document.body.querySelector('div').append(keyboard);
   // eslint-disable-next-line guard-for-in,no-restricted-syntax
   for (const key in keys) {
     const btn = document.createElement('div');
@@ -29,15 +28,31 @@ function drawBoard() {
 }
 drawBoard();
 
+function shift() {
+  if (document.querySelector('div[keycode=KeyA').innerText.charCodeAt() === 97
+    || document.querySelector('div[keycode=KeyA').innerText.charCodeAt() === 65) {
+    // eslint-disable-next-line no-unused-expressions
+    (document.querySelector('div[keycode=KeyA').innerText.charCodeAt() !== 97)
+      ? keyboard.style.textTransform = 'none'
+      : keyboard.style.textTransform = 'capitalize';
+  }
+  if (document.querySelector('div[keycode=KeyA').innerText.charCodeAt() === 1060
+    || document.querySelector('div[keycode=KeyA').innerText.charCodeAt() === 1092) {
+    // eslint-disable-next-line no-unused-expressions
+    (document.querySelector('div[keycode=KeyA').innerText.charCodeAt() !== 1092)
+      ? keyboard.style.textTransform = 'none'
+      : keyboard.style.textTransform = 'capitalize';
+  }
+}
+
 
 document.addEventListener('keydown', (event) => {
-  const keyboard = document.body.querySelector('.keyboard');
+  // const keyboard = document.body.querySelector('.keyboard');
   const button = keys.find((btn) => btn.code === event.code);
   const key = document.querySelector(`div[keycode=${button.code}]`);
-  console.log(button.en.length);
   key.style.background = 'linear-gradient(rgb(250, 250, 250), rgb(150, 160, 170))';
   if (button[appState.lang].length === 1) {
-    document.body.querySelector('textarea').insertAdjacentText('beforeend', `${key.innerText}`);
+    text.value += `${key.innerText}`;
   }
   switch (button.code) {
     case 'CapsLock':
@@ -51,11 +66,22 @@ document.addEventListener('keydown', (event) => {
       // eslint-disable-next-line no-unused-expressions
       if (appState.shiftFlag === 'off') {
         appState.shiftFlag = 'on';
-        // eslint-disable-next-line no-unused-expressions
-        (document.querySelector('div[keycode=KeyA').innerText.charCodeAt() < 97)
-          ? keyboard.style.textTransform = 'none'
-          : keyboard.style.textTransform = 'capitalize';
+        shift();
       }
+      // eslint-disable-next-line guard-for-in,no-restricted-syntax
+      break;
+    case 'Tab':
+      event.preventDefault();
+      text.value += '  ';
+      console.log(appState.lang);
+      break;
+    case 'Backspace':
+      // eslint-disable-next-line no-case-declarations,no-unused-vars
+      const string = document.querySelector('textarea');
+      string.value = string.value.substr(0, string.value.length - 1);
+      break;
+    case 'Enter':
+      text.value += '\n';
       break;
     default:
       break;
@@ -64,31 +90,97 @@ document.addEventListener('keydown', (event) => {
 
 document.addEventListener('keyup', (event) => {
   // eslint-disable-next-line no-unused-vars
-  const keyboard = document.body.querySelector('.keyboard');
   const button = keys.find((btn) => btn.code === event.code);
   const key = document.querySelector(`div[keycode=${button.code}]`);
   key.style.background = 'rgb(243, 243, 243)';
-  console.log(button.code);
-  // eslint-disable-next-line no-unused-expressions
-  if (button.code === 'ShiftLeft' || button.code === 'ShiftRight') {
+  if (button.en === 'Shift') {
     appState.shiftFlag = 'off';
-    // eslint-disable-next-line no-unused-expressions
-    (document.querySelector('div[keycode=KeyA').innerText.charCodeAt() < 97)
-      ? keyboard.style.textTransform = 'none'
-      : keyboard.style.textTransform = 'capitalize';
+    shift();
   }
 });
 
 document.querySelector('.keyboard').addEventListener('click', (event) => {
   if (event.target.className !== 'keyboard' && event.target.innerText.length === 1) {
-    document.body.querySelector('textarea').insertAdjacentText('beforeend', `${event.target.innerText}`);
+    text.value += `${event.target.innerText}`;
+  }
+  const button = keys.find((btn) => btn.en === event.target.innerText);
+  switch (button.en) {
+    case 'Caps Lock':
+      // eslint-disable-next-line no-unused-expressions
+      keyboard.style.textTransform !== 'capitalize'
+        ? keyboard.style.textTransform = 'capitalize'
+        : keyboard.style.textTransform = 'none';
+      break;
+    case 'Tab':
+      event.preventDefault();
+      text.value += '  ';
+      break;
+    case 'Backspace':
+      // eslint-disable-next-line no-case-declarations,no-unused-vars
+      const string = document.querySelector('textarea');
+      string.value = string.value.substr(0, string.value.length - 1);
+      break;
+    case 'Enter':
+      text.value += '\n';
+      break;
+    default:
+      break;
   }
 });
 
-// document.addEventListener('keydown', (event) => {
-//   const button = keys.find((btn) => btn.code === event.code);
-// eslint-disable-next-line max-len
-//   if (button.code === 'CapsLock' && document.body.querySelector('.keyboard').style.textTransform !== 'capitalize') {
-//     document.body.querySelector('.keyboard').style.textTransform = 'capitalize';
-//   } else document.body.querySelector('.keyboard').style.textTransform = 'none';
-// });
+document.querySelector('.keyboard').addEventListener('mousedown', (event) => {
+  const button = keys.find((btn) => btn.en === event.target.innerText);
+  if (button.en === 'Shift') {
+    // eslint-disable-next-line no-unused-expressions
+    if (appState.shiftFlag === 'off') {
+      appState.shiftFlag = 'on';
+      shift();
+    }
+  }
+});
+
+document.querySelector('.keyboard').addEventListener('mouseup', (event) => {
+  const button = keys.find((btn) => btn.en === event.target.innerText);
+  if (button.code === 'CapsLock') console.log('batch');
+  if (button.code === 'ShiftRight' || button.code === 'ShiftLeft') {
+    appState.shiftFlag = 'off';
+    shift();
+  }
+});
+
+function runOnKeys(func, ...codes) {
+  const pressed = new Set();
+
+  document.addEventListener('keydown', (event) => {
+    pressed.add(event.code);
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const code of codes) { // все ли клавиши из набора нажаты?
+      if (!pressed.has(code)) {
+        return;
+      }
+    }
+
+    // да, все
+
+    // во время показа alert, если посетитель отпустит клавиши - не возникнет keyup
+    // при этом JavaScript "пропустит" факт отпускания клавиш, а pressed[keyCode] останется true
+    // чтобы избежать "залипания" клавиши -- обнуляем статус всех клавиш, пусть нажимает всё заново
+
+    pressed.clear();
+    // eslint-disable-next-line no-unused-expressions
+    func() === 'ru' ? appState.lang = 'en' : appState.lang = 'ru';
+    keyboard.innerHTML = '';
+    drawBoard();
+  });
+
+  document.addEventListener('keyup', (event) => {
+    pressed.delete(event.code);
+  });
+}
+
+runOnKeys(
+  () => appState.lang,
+  'ControlLeft',
+  'AltLeft',
+);
--- src/keys.js
+++ src/keys.js
@@ -1,17 +1,43 @@
 const keys = [
-  { code: 'Backquote', ru: 'ё', en: '`' },
-  { code: 'Digit1', ru: '1', en: '1' },
-  { code: 'Digit2', ru: '2', en: '2' },
-  { code: 'Digit3', ru: '3', en: '3' },
-  { code: 'Digit4', ru: '4', en: '4' },
-  { code: 'Digit5', ru: '5', en: '5' },
-  { code: 'Digit6', ru: '6', en: '6' },
-  { code: 'Digit7', ru: '7', en: '7' },
-  { code: 'Digit8', ru: '8', en: '8' },
-  { code: 'Digit9', ru: '9', en: '9' },
-  { code: 'Digit0', ru: '0', en: '0' },
-  { code: 'Minus', ru: '-', en: '-' },
-  { code: 'Equal', ru: '=', en: '=' },
+  {
+    code: 'Backquote', ru: 'ё', en: '`', enSpec: '~',
+  },
+  {
+    code: 'Digit1', ru: '1', en: '1', enSpec: '!', ruSpec: '!',
+  },
+  {
+    code: 'Digit2', ru: '2', en: '2', enSpec: '@', ruSpec: '"',
+  },
+  {
+    code: 'Digit3', ru: '3', en: '3', enSpec: '#', ruSpec: '№',
+  },
+  {
+    code: 'Digit4', ru: '4', en: '4', enSpec: '$', ruSpec: ';',
+  },
+  {
+    code: 'Digit5', ru: '5', en: '5', enSpec: '%', ruSpec: '%',
+  },
+  {
+    code: 'Digit6', ru: '6', en: '6', enSpec: '^', ruSpec: ':',
+  },
+  {
+    code: 'Digit7', ru: '7', en: '7', enSpec: '&', ruSpec: '?',
+  },
+  {
+    code: 'Digit8', ru: '8', en: '8', enSpec: '*', ruSpec: '*',
+  },
+  {
+    code: 'Digit9', ru: '9', en: '9', enSpec: '(', ruSpec: '(',
+  },
+  {
+    code: 'Digit0', ru: '0', en: '0', enSpec: ')', ruSpec: ')',
+  },
+  {
+    code: 'Minus', ru: '-', en: '-', enSpec: '_', ruSpec: '_',
+  },
+  {
+    code: 'Equal', ru: '=', en: '=', enSpec: '+', ruSpec: '+',
+  },
   { code: 'Backspace', ru: 'Backspace', en: 'Backspace' },
   { code: 'Tab', ru: 'Tab', en: 'Tab' },
   { code: 'KeyQ', ru: 'й', en: 'q' },
@@ -24,9 +50,15 @@ const keys = [
   { code: 'KeyI', ru: 'ш', en: 'i' },
   { code: 'KeyO', ru: 'щ', en: 'o' },
   { code: 'KeyP', ru: 'з', en: 'p' },
-  { code: 'BracketLeft', ru: 'х', en: '[' },
-  { code: 'BracketRight', ru: 'ъ', en: ']' },
-  { code: 'Backslash', ru: '\\', en: '\\' },
+  {
+    code: 'BracketLeft', ru: 'х', en: '[', enSpec: '{',
+  },
+  {
+    code: 'BracketRight', ru: 'ъ', en: ']', enSpec: '}',
+  },
+  {
+    code: 'Backslash', ru: '\\', en: '\\', enSpec: '|', ruSpec: '/',
+  },
   { code: 'Delete', ru: 'Del', en: 'Del' },
   { code: 'CapsLock', ru: 'Caps Lock', en: 'Caps Lock' },
   { code: 'KeyA', ru: 'ф', en: 'a' },
@@ -38,8 +70,12 @@ const keys = [
   { code: 'KeyJ', ru: 'о', en: 'j' },
   { code: 'KeyK', ru: 'л', en: 'k' },
   { code: 'KeyL', ru: 'д', en: 'l' },
-  { code: 'Semicolon', ru: 'ж', en: ';' },
-  { code: 'Quote', ru: 'э', en: '"' },
+  {
+    code: 'Semicolon', ru: 'ж', en: ';', enSpec: ':',
+  },
+  {
+    code: 'Quote', ru: 'э', en: '"', enSpec: '"',
+  },
   { code: 'Enter', ru: 'Enter', en: 'Enter' },
   { code: 'ShiftLeft', ru: 'Shift', en: 'Shift' },
   { code: 'KeyZ', ru: 'я', en: 'z' },
@@ -49,9 +85,15 @@ const keys = [
   { code: 'KeyB', ru: 'и', en: 'b' },
   { code: 'KeyN', ru: 'т', en: 'n' },
   { code: 'KeyM', ru: 'ь', en: 'm' },
-  { code: 'Comma', ru: 'б', en: ',' },
-  { code: 'Dot', ru: 'ю', en: '.' },
-  { code: 'Slash', ru: '.', en: '/' },
+  {
+    code: 'Comma', ru: 'б', en: ',', enSpec: '<',
+  },
+  {
+    code: 'Dot', ru: 'ю', en: '.', enSpec: '>',
+  },
+  {
+    code: 'Slash', ru: '.', en: '/', enSpec: '?', ruSpec: ',',
+  },
   { code: 'ArrowUp', ru: '↑', en: '↑' },
   { code: 'ShiftRight', ru: 'Shift', en: 'Shift' },
   { code: 'ControlLeft', ru: 'Ctrl', en: 'Ctrl' },
